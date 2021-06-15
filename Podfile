f229727b1309ff2bf35d74aabe5550f1d4317392
meafs <abrar.fahim@yandex.com>
2021-06-15 07:47:41 -0700

    Add toast

--- Podfile
+++ Podfile
@@ -6,6 +6,9 @@ target 'SUSTcast' do
     pod 'Firebase/Auth'
     pod 'Firebase/Core'
     pod 'Firebase/Firestore'
+    pod 'Toaster'
+
+    
 
 
 end
--- SUSTcast/View Controllers/HomeViewController.swift
+++ SUSTcast/View Controllers/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Toaster
 
 class HomeViewController: UIViewController {
 
@@ -13,6 +14,8 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        Toast(text: "Home").show()
     }
     
 
