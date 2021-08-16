b8586cc6804d515e236ef76720db37ef25f4365f
meafs <abrar.fahim@yandex.com>
2021-08-16 10:13:06 -0700

    Audio control, playback test

--- SUSTcast/View Controllers/StreamViewController.swift
+++ SUSTcast/View Controllers/StreamViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 import Firebase
 import AVFoundation
 import Toaster
+import  MediaPlayer
+import AVKit
 
 class StreamViewController: UIViewController {
     
     var ref: DatabaseReference!
     var player: AVPlayer!
+
+    //private let audioSession = AVAudioSession.sharedInstance()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,7 +47,19 @@ class StreamViewController: UIViewController {
         
         
        // var playing : Bool? = false
-        let urlString = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+       // let urlString = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+        let urlString = "http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8"
+        //let urlString = "http://aler.org:8000/radiorunacunapac.ogg.m3u"
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+                try AVAudioSession.sharedInstance().setActive(true)
+                debugPrint("AVAudioSession is Active and Category Playback is set")
+                UIApplication.shared.beginReceivingRemoteControlEvents()
+            setupCommandCenter()
+            } catch {
+                debugPrint("Error: \(error)")
+            }
         guard let url = URL.init(string: urlString)
                else {
                    return
@@ -51,12 +67,28 @@ class StreamViewController: UIViewController {
            let playerItem = AVPlayerItem.init(url: url)
            player = AVPlayer.init(playerItem: playerItem)
     
-           //player.play()
-           //print("Playing")
+           player.play()
+           print("Playing")
        
     
         //self.ref.child("IcecastServer").obser
     
+    }
+    
+    private func setupCommandCenter() {
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = [MPMediaItemPropertyTitle: "SUSTcast"]
+
+        let commandCenter = MPRemoteCommandCenter.shared()
+        commandCenter.playCommand.isEnabled = true
+        commandCenter.pauseCommand.isEnabled = true
+        commandCenter.playCommand.addTarget { [weak self] (event) -> MPRemoteCommandHandlerStatus in
+            self?.player.play()
+            return .success
+        }
+        commandCenter.pauseCommand.addTarget { [weak self] (event) -> MPRemoteCommandHandlerStatus in
+            self?.player.pause()
+            return .success
+        }
     }
 
 }
